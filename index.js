ba6ab0514e14abbc1e01698e671c3545d03abed7
febrian <fmagusta@tigaserangkai.co.id>
2018-06-11 09:19:45 +0700

    -

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const password = 'fmasecret1'
+//yg login masih pakai google, silahkan di forget password biar dibuatkan password lazada
+//login dengan google blm tak set pakai bot ini karena butuh cookies
+//Lazada tidak menggunakan #id di element nya, jadi selector pakai caption (xpath) & css class
+//sesuaikan selector apabila kelak lazada ada update interface
+
+//ket :
+//1. proses login
+//2. clear existing cart item
+//3. go to product url, refresh smpai bisa ditambahkan ke TROLI
+//4. tambahkan ke TROLI & checkout
+//5. pembayaran, sukses loop ke step no 2 sejumlah products array
+
 const user = '7.sourcecode@gmail.com'
-// const product_url = 'https://www.lazada.co.id/products/xiaomi-redmi-5a-grey-snapdragron-425-quad-core-i336294944-s346595741.html'
+const password = 'fmasecret1'
+const paymentmethod = 'COD';
+//COD; Mandiri; BCA; dst, ,hati2 case sensitive
+
+//bot akan meloop proses FS sejumlah array dibawah,
+//misal ingin 2x proses produk yg sama, inputkan 2x di array dibawah dengan format {name:'name',url:'url'}
 const products = [
 	{name:'5A Grey', url:'https://www.lazada.co.id/products/xiaomi-mi-power-bank-10000mah-silver-i160040604-s181911598.html'}
 	// {name:'5A Gold', url:'https://www.lazada.co.id/products/xiaomi-redmi-5a-gold-snapdragron-425-quad-core-i160043545-s181917409.html'}
@@ -9,8 +25,11 @@ const products = [
 const puppeteer = require('puppeteer')
 const loginurl = 'https://member.lazada.co.id/user/login'
 const carturl = 'https://cart.lazada.co.id/cart'
+
+// silahkan aktif kan di puppeteer.launch parameter jika ingin menggunakan browser & existing profile yg telah terinstall
 // const chromepath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
 // const newchromeprofile = 'C:/Users/fmanda/AppData/Local/Google/Chrome/User Data/'
+
 const dialog_cart_selector = '#dialog-body-1'
 const btn_del_cart = 'span.lazada.lazada-ic-Delete.lazada-icon.icon.delete';
 const xpath_del_cart = '//button[contains(.,"HAPUS")]'
@@ -76,14 +95,19 @@ async function doPayment(page, product){
 			var button = (await page.$x('//button[contains(.,"LANJUTKAN KE PEMBAYARAN")]'))[0];
 			await Promise.all([ button.click(), page.waitForNavigation({ waitUntil: 'networkidle2' }) ]);
 
-			var button = (await page.$x('//div[.="melalui bank transfer"]'))[0];
-			await Promise.all([ button.click(), page.waitForSelector('div.checkbox-list.item-content') ]);
+			if (paymentmethod == 'COD'){
+				var button = (await page.$x('//div[.="Bayar di Tempat"]'))[0];
+				await Promise.all([ button.click(), page.waitForSelector('div.checkbox-list.item-content') ]);
+			}else{
+				var button = (await page.$x('//div[.="melalui bank transfer"]'))[0];
+				await Promise.all([ button.click(), page.waitForSelector('div.checkbox-list.item-content') ]);
 
-			var button = (await page.$x('//p[contains(@class, "bank-name") and text() = "Mandiri"]'))[0];
-			await button.click()
+				var button = (await page.$x('//p[contains(@class, "bank-name") and text() = "'+ paymentmethod + '"]'))[0];
+				await button.click()
+			}
 
-      var button = (await page.$x('//button[contains(.,"BUAT PESANAN SEKARANG")]'))[0];
-			await Promise.all([ button.click(), page.waitForNavigation({ waitUntil: 'networkidle2' }) ]);
+      // var button = (await page.$x('//button[contains(.,"BUAT PESANAN SEKARANG")]'))[0];
+			// await Promise.all([ button.click(), page.waitForNavigation({ waitUntil: 'networkidle2' }) ]);
 
 			break;
 		}catch(err){
